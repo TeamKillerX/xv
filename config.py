70859d1d6abbb067076989247a9b2c68ec2d0c92
Randy Dev <killerx@randydev.my.id>
2025-08-14 00:16:39 +0700

    _

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv()
 API_ID = getenv("API_ID")
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
+MONGO_URL = getenv("MONGO_URL")
 
-if not all([API_ID, API_HASH, BOT_TOKEN]):
+if not all([API_ID, API_HASH, BOT_TOKEN, MONGO_URL]):
     raise ValueError("Missing one or more essential environment variables.")
