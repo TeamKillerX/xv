6f49982d99379c84def4ff47c420c1b3c75ea350
Randy Dev <killerx@randydev.my.id>
2025-08-14 00:49:12 +0700

    _

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ API_ID = getenv("API_ID")
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_URL = getenv("MONGO_URL")
+GEMINI_API_KEY = getenv("GEMINI_API_KEY")
 
-if not all([API_ID, API_HASH, BOT_TOKEN, MONGO_URL]):
+if not all([API_ID, API_HASH, BOT_TOKEN, MONGO_URL, GEMINI_API_KEY]):
     raise ValueError("Missing one or more essential environment variables.")
